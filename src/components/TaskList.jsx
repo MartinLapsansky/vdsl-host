1ce2389ca5b75013f33a497e6d7a91589a763c5c
Martin Lapsansky <martin.lapsansky@student.tuke.sk>
2025-01-04 13:40:46 +0100

    Fix CORS issue and update API URL

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -15,7 +15,7 @@ const TaskList = () => {
     const [finalAnswer, setFinalAnswer] = useState('');
     const [showCelebration, setShowCelebration] = useState(false);
 
-    axios.defaults.baseURL = 'https://30b9-78-99-33-3.ngrok-free.app';
+    axios.defaults.baseURL = 'https://dca8-78-99-33-3.ngrok-free.app';
 
     useEffect(() => {
         // Fetch tasks
